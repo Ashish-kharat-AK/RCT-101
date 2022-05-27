5d97de3e14bea4c300c165771156bd75c1f2b631
Ashsih Kharat <87631498+Ashish-kharat-AK@users.noreply.github.com>
2022-05-27 11:33:06 +0530

    Add files via upload
    uploaded

---
+++ day-4/react-todos/src/components/TodoInput.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+export const TodoInput = ({getData}) => {
+    const [ todo, setTodo ] = useState("");
+
+    console.log("todo", todo);
+    return (
+        <div className='inputForm'>
+            <input type="text" className='input' placeholder="Enter Task" onChange={ (e) => setTodo(e.target.value) } />
+            <button onClick={ () => { getData(todo); } }>Add Task</button>
+        </div>
+    );
+}
+export default TodoInput
